750f40bf782f6a9cc430ea09cdb80e406b65a0c7
ducc8201 <57596660+ducc8201@users.noreply.github.com>
2019-11-18 22:12:03 +0700

    Update Map1.java

--- src/ObjectsGame/Map1.java
+++ src/ObjectsGame/Map1.java
@@ -8,13 +8,17 @@ public class Map1 extends GameEntity {
     public Map1(){}
 
     public String[][] MAP_SPRITES = new String[][] {
-            { "024", "024", "003", "047", "047", "047", "004", "024", "024", "024" },
-            { "024", "024", "025", "299", "001", "002", "023", "024", "024", "024" },
-            { "024", "024", "025", "023", "024", "025", "023", "024", "024", "024" },
-            { "003", "047", "048", "023", "024", "025", "023", "024", "024", "024" },
-            { "025", "299", "001", "027", "024", "025", "046", "047", "047", "047" },
-            { "025", "023", "024", "024", "024", "026", "001", "001", "001", "001" },
-            { "025", "023", "024", "024", "024", "024", "024", "024", "024", "024" },
+            { "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024"},
+            { "003", "047", "047", "047", "047", "047", "004", "024", "024", "024", "024", "024", "024", "024", "024", "024"},
+            { "025", "299", "001", "001", "001", "002", "023", "024", "024", "024", "210", "254", "254", "254", "185", "185"},
+            { "025", "023", "024", "024", "024", "025", "023", "024", "024", "024", "232", "207", "208", "208", "139", "139"},
+            { "025", "023", "024", "024", "024", "025", "023", "024", "024", "024", "232", "230", "024", "024", "024", "024"},
+            { "025", "023", "024", "024", "024", "025", "023", "024", "024", "024", "232", "230", "024", "024", "024", "024"},
+            { "025", "023", "024", "024", "024", "025", "046", "047", "047", "047", "048", "023", "024", "024", "024", "024"},
+            { "025", "023", "024", "024", "024", "026", "001", "001", "001", "001", "001", "027", "024", "024", "024", "024"},
+            { "025", "023", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024"},
+            { "025", "023", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024"},
+            { "025", "023", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024", "024"},
     };
 
     @Override
@@ -28,23 +32,5 @@ public class Map1 extends GameEntity {
                 g2d.drawImage(img, j * 64, i * 64, null);
             }
         }
-
-        Image img7 = new ImageIcon(getClass().getResource("/Defaultsize/bkg" + ".png")).getImage();
-        Image img = new ImageIcon(getClass().getResource("/Defaultsize/towerDefense_tile" + "130" + ".png")).getImage();
-        Image img1 = new ImageIcon(getClass().getResource("/Defaultsize/towerDefense_tile" + "132" + ".png")).getImage();
-        Image img2 = new ImageIcon(getClass().getResource("/Defaultsize/towerDefense_tile" + "020" + ".png")).getImage();
-        Image img3 = new ImageIcon(getClass().getResource("/Defaultsize/towerDefense_tile" + "131" + ".png")).getImage();
-        Image img4 = new ImageIcon(getClass().getResource("/Defaultsize/towerDefense_tile" + "020" + ".png")).getImage();
-        Image img5 = new ImageIcon(getClass().getResource("/Defaultsize/towerDefense_tile" + "136" + ".png")).getImage();
-        Image img6 = new ImageIcon(getClass().getResource("/Defaultsize/towerDefense_tile" + "132" + ".png")).getImage();
-
-        g2d.drawImage(img7, 640,0, null );
-        g2d.drawImage(img, 0 , 0, null);
-        g2d.drawImage(img1, 194 , 360, null);
-        g2d.drawImage(img2, 0 , 0, null);
-        g2d.drawImage(img3, 466 , 370, null);
-        g2d.drawImage(img4, 466 , 370, null);
-        g2d.drawImage(img5, 512 , 64, null);
-        g2d.drawImage(img6, 520 , 45, null);
     }
 }
