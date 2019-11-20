526e35e73e3997200fdf398ac2b3414a21aff57a
ntdat09 <53997502+ntdat09@users.noreply.github.com>
2019-11-20 22:03:15 +0700

    Update Tower.java

--- src/ObjectsGame/Tower.java
+++ src/ObjectsGame/Tower.java
@@ -7,6 +7,7 @@ public abstract class Tower extends GameEntity {
     protected double dame;
     protected double gunspeed;
     protected double shootRange;
+    public static double radian = 0;
 
     public double getDame() {
         return dame;
@@ -18,6 +19,13 @@ public abstract class Tower extends GameEntity {
 
     public double getShootRange() {
         return shootRange;
+    }
+
+    public double getRadian(Enemy e) {
+        Point a = new Point((int) e.x - (int) this.x, (int) e.y -(int) this.y);
+        Point b = new Point(0, 1);
+        radian = Math.acos((a.x*b.x + a.y*b.y)/(Math.sqrt(a.x*a.x + a.y*a.y) + Math.sqrt(b.x*b.x + b.y*b.y)));
+        return radian;
     }
 
     @Override
