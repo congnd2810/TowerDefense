3abaf1bb68bcdbda8c290c4686969fd78ad5b7e8
ducanh170920 <56523371+ducanh170920@users.noreply.github.com>
2019-11-24 11:54:53 +0700

    Update Tower.java

--- src/ObjectsGame/Tower.java
+++ src/ObjectsGame/Tower.java
@@ -1,13 +1,15 @@
 package ObjectsGame;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 
 public abstract class Tower extends GameEntity {
 
     protected double dame;
     protected double gunspeed;
     protected double shootRange;
-    public static double radian = 0;
+    protected Image base;
+    public double radian = 0;
 
     public double getDame() {
         return dame;
@@ -21,20 +23,41 @@ public abstract class Tower extends GameEntity {
         return shootRange;
     }
 
-    public double getRadian(Enemy e) {
-        Point a = new Point((int) e.x - (int) this.x, (int) e.y -(int) this.y);
-        Point b = new Point(0, 1);
-        radian = Math.acos((a.x*b.x + a.y*b.y)/(Math.sqrt(a.x*a.x + a.y*a.y) + Math.sqrt(b.x*b.x + b.y*b.y)));
-        return radian;
+    public double getRadian() {return  (radian/Math.PI) * 180;}
+
+    public void setRadian(Enemy e) {
+        Point a = new Point((int) e.x + 32 - (int) (this.x + 32), (int) e.y + 32 - (int) (this.y + 32));
+        Point b = new Point(0, -1);
+        if (a.x < 0) {
+            radian = -Math.acos((a.x*b.x + a.y*b.y)/(Math.sqrt(a.x*a.x + a.y*a.y) + Math.sqrt(b.x*b.x + b.y*b.y)));
+        }
+        else {
+            radian = Math.acos((a.x*b.x + a.y*b.y)/(Math.sqrt(a.x*a.x + a.y*a.y) + Math.sqrt(b.x*b.x + b.y*b.y)));
+        }
+    }
+
+    public  abstract  void shoot(Enemy e);
+
+    public void setRadian(double R) {
+        radian = R;
     }
 
     @Override
-    public void update() {
+    public void render(Graphics2D graphics2D) {
+        int i =(int) x;
+        int j = (int) y;
+        int shot = (int) shootRange + 42;
 
-    }
+        setBounds(i+32-shot, j+32-shot, shot*2, shot*2);
 
-    @Override
-    public void render(Graphics2D g2d) {
+        graphics2D.drawImage(base, i, j, null);
 
+        AffineTransform backup = graphics2D.getTransform();
+        AffineTransform a = AffineTransform.getRotateInstance(radian , i+32, j+32);
+        graphics2D.setTransform(a);
+
+        graphics2D.drawImage(img, i, j, null);
+
+        graphics2D.setTransform(backup);
     }
 }
