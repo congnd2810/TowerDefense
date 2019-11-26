d4ea0e573ce3a602cc5fb28cddeeef920f854a70
ducc8201 <57596660+ducc8201@users.noreply.github.com>
2019-11-26 19:47:50 +0700

    Update Enemy.java

--- src/ObjectsGame/Enemy.java
+++ src/ObjectsGame/Enemy.java
@@ -4,33 +4,50 @@ import java.awt.*;
 
 public abstract class Enemy extends GameEntity {
 
-    protected double health;
+    protected int health;
     protected double speed;
-    protected double armor;
+    protected int armor;
     protected int reward;
+    protected Map map;
     protected Direction direction;
 
-    public void updateDirection() {
+    public void setMap(Map map) {
+        this.map = map;
+    }
 
-        if (x == 32 && y > 160) {
-            direction = Direction.UP;
+    public void updateDirection() {
+        if (map.numberOfMap == 1) {
+            if (x == 32 && y > 96) {
+                direction = Direction.UP;
+            }
+            else if (y == 96 && x < 352) {
+                direction = Direction.RiGHT;
+            }
+            else if (x == 352 && y < 416) {
+                direction = Direction.DOWN;
+            }
+            else if (x < 672 && y == 416) {
+                direction = Direction.RiGHT;
+            }
+            else if (x == 672 && y > 160) {
+                direction = Direction.UP;
+            }
+            else if (y == 160 && x <= 960) {
+                direction = Direction.RiGHT;
+            }
+            else direction = Direction.LEFT;
         }
-        else if (y == 160 && x < 352) {
-            direction = Direction.RIGHT;
+        else {
+            if (x == 32 && y > 352) {
+                direction = Direction.UP;
+            }
+            else if (y == 352 && x < 864) {
+                direction = Direction.RiGHT;
+            }
+            else if (x == 864 && y > 0) {
+                direction = Direction.UP;
+            }
         }
-        else if (x == 352 && y < 480) {
-            direction = Direction.DOWN;
-        }
-        else if (x < 672 && y == 480) {
-            direction = Direction.RIGHT;
-        }
-        else if (x == 672 && y > 224) {
-            direction = Direction.UP;
-        }
-        else if (y == 224 && x <= 960) {
-            direction = Direction.RIGHT;
-        }
-        else if (x > 960) direction = Direction.LEFT;
     }
 
     @Override
@@ -45,14 +62,12 @@ public abstract class Enemy extends GameEntity {
                 break;
             case LEFT:
                 break;
-            case RIGHT:
+            case RiGHT:
                 x += speed;
                 break;
         }
     }
 
     @Override
-    public void render(Graphics2D g2d) {
-
-    }
+    public void render(Graphics2D g2d) {}
 }
